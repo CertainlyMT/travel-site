bc4a9c3170abb494f8491a8f9f540cf0377a3346
Mike <mailnaycuatui11@gmail.com>
2017-12-18 12:13:12 +0700

    Chapter 7.1: Mobile-first and apply to font-size of large-hero title

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars')
 nested = require('postcss-nested'),
+mixins = require('postcss-mixins'),
 cssimport = require('postcss-import'); /* use @import in main file */
 
 gulp.task('styles', function() {
 	//manipilate source style.css through pipe() to the destination
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssimport, cssvars, nested, autoprefixer])) //postcss expect a array
+		.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer])) //postcss expect a array
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end'); // this tell the watch task that styles task have com to an end, not the gulp. However, we can not see the error message
